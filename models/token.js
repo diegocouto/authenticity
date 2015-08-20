195446ab36f04e08e9e11643c567ddd5ead563b4
Diego Couto <diego.gcouto@gmail.com>
2015-08-20 18:00:51 -0300

    Token creation restrictions defined.

--- models/token.js
+++ models/token.js
@@ -1,10 +1,25 @@
 'use strict';
 
+var hat = require('hat');
+
 module.exports = function(sequelize, DataTypes) {
   var Token = sequelize.define('Token', {
-    key: DataTypes.STRING,
-    service: DataTypes.STRING
+    key: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    service: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    classMethods: {
+      generate: function(service, callback) {
+           
+      }
+    }
   });
-  
-  return Token;
+
+return Token;
 };
--- test/model/token.test.js
+++ test/model/token.test.js
@@ -1,10 +1,27 @@
-var expect = require('chai').expect;
+var chai = require("chai");  
+var chaiAsPromised = require("chai-as-promised");  
+chai.use(chaiAsPromised);  
+chai.should();
 
 var app = require('../../app');
 
 describe('Token', function() {
+  beforeEach(function () {
+    this.Token = require('../../models').Token;
+  });
+
   describe('on token validation', function(){
-    it('returns null if service is not defined.');
+    it('throw exception when creating without a service.', function(done){
+      this.Token.create({key: 'simple_key'}).should.be.rejectedWith('service cannot be null').notify(done);
+    });
+
+    it('throw exception when creating a non-unique key', function(done){
+      token = this.Token;
+
+      token.create({key: 'simple_key', service: 'service'}).then(function(valid_token) {
+        token.create({key: valid_token.key, service: valid_token.service}).should.be.rejectedWith('service cannot be null').notify(done);
+      });
+    });
   });
 
   describe('on successful creation', function() {
