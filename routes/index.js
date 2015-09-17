38d585c8bc9f04fb1275eb44e7fe64940015391b
Diego Couto <diego.gcouto@gmail.com>
2015-09-17 15:40:23 -0300

    Download route simplified.

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,12 @@ router.get('/', function(req, res, next) {
   res.render('index', {title: 'Authenticity'});
 });
 
-router.get('/files/:file_key/download', function(req, res, next) {
+router.get('/:file_key', function(req, res, next) {
   File.findOne({where: {key: req.params.file_key}}).then(function(file) {
     if(file)
       return res.download(file.path);
 
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    next();
   });
 });
 
